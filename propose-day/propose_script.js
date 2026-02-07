a27b454d26ca26091a505de691c04ad3a981300f
kalravivaan <kalravivaan1@gmail.com>
2026-02-08 01:21:27 +0530

    Update propose_script.js

--- propose-day/propose_script.js
+++ propose-day/propose_script.js
@@ -6,18 +6,17 @@ function unlockPropose() {
   const input = document.getElementById("passwordInput").value.trim();
   const error = document.getElementById("error");
 
-  if (input === PROPOSE_PASSWORD) {
-    // remember unlock for this session
-    sessionStorage.setItem("proposeUnlocked", "true");
-
-    // go to intro page
+  if (input === "VivaanLovesTvisha") {
+    localStorage.setItem("valentineProgress", "2");
     window.location.href = "propose_intro.html";
   } else {
     error.textContent = "Wrong password 🥺 Try again!";
   }
 }
 
+
 // Called from propose_intro.html
 function goToLetter() {
   window.location.href = "propose_letter.html";
 }
+
